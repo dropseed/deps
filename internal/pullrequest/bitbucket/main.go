a8f3f4861606c02d5a969b96e1d77ff7890095c2
Dave Gaeddert <dave.gaeddert@gmail.com>
2019-09-09 17:09:51 -0500

    Remove bitbucket update PR

--- internal/pullrequest/bitbucket/main.go
+++ internal/pullrequest/bitbucket/main.go
@@ -3,11 +3,9 @@ package bitbucket
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"regexp"
 
 	"github.com/dropseed/deps/internal/config"
 	"github.com/dropseed/deps/internal/output"
@@ -87,39 +85,9 @@ func (pr *PullRequest) CreateOrUpdate() error {
 	if resp.StatusCode == 201 {
 		output.Event("Successfully created Bitbucket pull request for %v\n", pr.ProjectAPIURL)
 		return nil
-	} else if resp.StatusCode == 409 {
-		output.Event("pull request already exists")
-		var data map[string][]string
-		if err := json.Unmarshal([]byte(body), &data); err != nil {
-			return err
-		}
-
-		if message, hasMessage := data["message"]; hasMessage {
-			pattern := regexp.MustCompile("!(\\d+)")
-			matches := pattern.FindStringSubmatch(message[0])
-			// finds !18 and 18...
-			if len(matches) != 2 {
-				return errors.New("Unable to find ID for existing pull request to update")
-			}
-			mrID := matches[1]
-			return pr.update(mrID, pullrequestData)
-		}
 	}
 
 	return fmt.Errorf("Failed to create pull request: %s", body)
-}
-
-func (pr *PullRequest) update(id string, data []byte) error {
-	url := pr.ProjectAPIURL + "/pullrequests/" + id
-	resp, body, err := pr.request("PUT", url, data)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode >= 400 {
-		return fmt.Errorf("Error updating pull request:\n\n%s", body)
-	}
-	output.Success("Updated pull request %s", id)
-	return nil
 }
 
 func (pr *PullRequest) getPullRequestOptions() map[string]interface{} {
